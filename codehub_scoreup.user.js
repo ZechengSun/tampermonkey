993e634c5ba7de341827267ef152b367ab8ebd56
ZechengSun <39629524+ZechengSun@users.noreply.github.com>
2023-06-14 11:04:59 +0800

    Update codehub_scoreup.user.js

--- codehub_scoreup.user.js
+++ codehub_scoreup.user.js
@@ -6,6 +6,8 @@
 // @author       孙泽程
 // @include      *://codehub-g.huawei.com/*
 // @icon         https://s3-hc-dgg.hics.huawei.com/sam-pro/2021/8/lWX899464/1629454439031-456.png
+// @downloadURL  https://github.com/ZechengSun/tampermonkey/raw/main/codehub_scoreup.user.js
+// @updateURL    https://github.com/ZechengSun/tampermonkey/raw/main/codehub_scoreup.user.js
 // @grant        none
 // ==/UserScript==
 
